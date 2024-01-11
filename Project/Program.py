9b3aab09bd4610ac502662aa2152f1ef85bd9795
Hugo Santos <89364200+llZektorll@users.noreply.github.com>
2024-01-11 21:21:10 +0000

    V0

---
+++ Project/Program.py
@@ -0,0 +1,15 @@
+# 
+#.SYNOPSIS
+#    Project for the Algorithms and Programing
+#.DESCRIPTION 
+#    
+#.NOTES 
+#    Vertsion:   1.0
+#    Author: Hugo Santos (https://github.com/llZektorll)
+#    Creation Date: 2024-01-11 (YYYY-MM-DD)
+#    Change: Initial script development
+#.COMPONENT 
+#    Connection modules include 
+#.LINK 
+#    Script repository: https://github.com/llZektorll/MonopolyTeminal
+
