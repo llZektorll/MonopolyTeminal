191489f73c721d30e2bfec75ce1050a913ef92b9
Hugo Santos <89364200+llZektorll@users.noreply.github.com>
2024-01-19 02:14:13 +0000

    update

--- Project/Controller.py
+++ Project/Controller.py
@@ -1,5 +1,5 @@
 '''
-    Registration of users
+    Registration of users - Restisto(sim), validação de outro users (não)
     buy location
     build house
     pay rent
@@ -11,42 +11,90 @@
     definition of winning
 '''
 import random
-# Automatic Roll the Dice
-def rolldice():
-    global a,b
-    a = random.randint(1,6)
-    b = random.randint(1,6)
-    total = a + b
-    return total
-
-#Create Users
-def reg_player(name):
-    if check_player(players, name) == True:
-        return False
-    else:
-        player = {'Name': name, 'Wins': 0, 'Games': 0}
-        players.append(player)
-        return players
 
 
-def check_player(players, name):
-    for player in players:
-        if player['Name'] == name:
-            return True
-    return False
+def reg_player(players, player_name):
+    for player in player_name:
+        players = ({"name": player, "position": 0, "money": 1500})
+        print(f'Player {player} added correctly.')
+        print(players)
+    return players
 
 
-def escrita_ficheiros(file_1, dados):
+def ComprarPropriedade(CardPosition, Jogador):
+    if (SaldoJogadores <= 0):
+        print('Saldo insuficiente.')
 
-    arquivo = open(file_1, "w")
-    arquivo.write(dados)
-    arquivo.close()
+    if (Board_Position[CardPosition] != CardPosition):
+        print('Jogador não esta na Propriedade.')
+
+    if (Board_Position[CardPosition]["owner"] != Jogador):
+        print('Propriedade já tem proprietário.')
+
+    if (Board_Position[CardPosition]["owner"] == "Board"):
+        Board_Position[CardPosition]["owner"] == Jogador
+        print('Propriedade adquirida com sucesso.')
 
 
-def ler_ficheiro(file_1, dados):
+def ConstruirCasa(CardPosition, Jogador):
+    if (SaldoJogadores <= 0):
+        print('Saldo insuficiente.')
 
-    arquivo = open(file_1, encoding="utf8")
-    linhas = arquivo.read()
-    arquivo.close()
-    return linhas
+    if (Board_Position[CardPosition]["owner"] != Jogador):
+        print('não é proprietário.')
 
+    if (Board_Position[CardPosition]["house"] >= 5):
+        print('Propriedade já tem proprietário.')
+
+    if (Board_Position[CardPosition]["owner"] == Jogador):
+        print('Casa construida com sucesso.')
+
+
+def PagarAluguer(JogadorRecpetor, Jogador):
+    if (SaldoJogadores[Jogador] <= 0):
+        print('Saldo insuficiente.')
+
+    if (Board_Position[CardPosition]["owner"] != JogadorRecpetor):
+        print('Receptor não é proprietário.')
+
+    if (Board_Position[CardPosition]["owner"] == JogadorRecpetor):
+        SaldoJogadores[JogadorRecpetor] = "valor da casa"
+        print('Aluguer pago com sucesso.')
+
+
+def HipotecarPropriedade(CardPosition, Jogador):
+    if (Board_Position[CardPosition]["owner"] != Jogador):
+        print('não é proprietário.')
+
+    if (Board_Position[CardPosition]["Hipotecada"] == True):
+        print('Propriedade já esta hipotecada.')
+
+    if (Board_Position[CardPosition]["Hipotecada"] == False):
+        Board_Position[CardPosition]["Hipotecada"] = True
+        print('Propriedade hipotecada com sucesso.')
+
+
+def DeshipotecarPropriedade(CardPosition, Jogador):
+    if (Board_Position[CardPosition]["owner"] != Jogador):
+        print('não é proprietário.')
+
+    if (Board_Position[CardPosition]["Hipotecada"] == False):
+        print('Propriedade  não esta hipotecada.')
+
+    if (SaldoJogadores[Jogador] <= 0):
+        print('Saldo insuficiente.')
+
+    if (Board_Position[CardPosition]["Hipotecada"] == True):
+        Board_Position[CardPosition]["Hipotecada"] = False
+        print('Propriedade deshipotecada com sucesso.')
+
+
+def VenderCasa(CardPosition, Jogador):
+    if (Board_Position[CardPosition]["owner"] != Jogador):
+        print('não é proprietário.')
+
+    if (Board_Position[CardPosition]["house"] <= 0):
+        print('Sem casas para vender.')
+
+    if (Board_Position[CardPosition]["house"] < 0):
+        print('Casas vendida com sucesso.')
--- Project/View.py
+++ Project/View.py
@@ -1,30 +1,130 @@
-def main():
-    #region Variables
-    players = {}
-    board = 4 # 4x4
-    house = 5
-    board_id = (1,2,3,4,5,6,7,8,9,10,11,12)
-    bord_house = (3,5,7,9,11)
-    player_location = {'player1': 1, 'player2': 1, 'player3':5}
-    #endregion
-    #region menu
-    print(f'''
-##########################
-###  Jogo do Monopólio ###
-##########################
+from Controller import *
+import json
 
-1 - Novo Jogo
-2 - Registar Jogadores
-3 - Histórico de jogos
-''')
-    
-    #endregion menu
-    option = input(int(f'Selectione uma opção: '))
-    if option == 1:
-        print ('test')
-    elif option == 2:
-        print ('test 2')
-    elif option == 3:
-        print ('test 3')
-    else :
-        print(f'Opção selecionada não é válida!')
+
+def main():
+    # region Variables
+    players = []
+    player_name = []
+    board = 4  # 4x4
+    house = 5
+    board_position = {
+        0: {
+            "position": "0",
+            "name": "Casa de Partida",
+            "color": "white",
+            "price": "200",
+            "owner": "Board"},
+        1: {
+            "position": "1",
+            "name": "Campo Grande",
+            "color": "Purple",
+            "price": "60",
+            "owner": "Board"},
+        2: {
+            "position": "2",
+            "name": "Caixa da Comunidade",
+            "color": "White",
+            "price": "0",
+            "owner": "Board"},
+        3: {
+            "position": "3",
+            "name": "Rua Faria Guimarães",
+            "color": "Purple",
+            "price": "60",
+            "owner": "Board"},
+        4: {
+            "position": "4",
+            "name": "Pague imposto sobre Capitais",
+            "color": "White",
+            "price": "200",
+            "owner": "Board"},
+        5: {
+            "position": "5",
+            "name": "Estação do Rossio",
+            "color": "Black",
+            "price": "200",
+            "owner": "Board"},
+        6: {
+            "position": "6",
+            "name": "Alamenda das linhas de Torres",
+            "color": "Cyan",
+            "price": "100",
+            "owner": "Board"},
+        7: {
+            "position": "7",
+            "name": "Sorte",
+            "color": "White",
+            "price": "0",
+            "owner": "Board"},
+        8: {
+            "position": "8",
+            "name": "Avenida das Nações Unidas",
+            "color": "Cyan",
+            "price": "100",
+            "owner": "Board"},
+        9: {
+            "position": "9",
+            "name": "Avenida 24 de Julho",
+            "color": "Cyan",
+            "price": "120",
+            "owner": "Board"},
+        10: {
+            "position": "10",
+            "name": "Cadeia",
+            "color": "White",
+            "price": "0",
+            "owner": "Board"},
+        11: {
+            "position": "11",
+            "name": "Avenida Central",
+            "color": "Purple",
+            "price": "140",
+            "owner": "Board"},
+        12: {
+            "position": "12",
+            "name": "Companhia de Electricidade",
+            "color": "White",
+            "price": "150",
+            "owner": "Board"},
+        13: {
+            "position": "13",
+            "name": "Rua Ferreira Borges",
+            "color": "Purple",
+            "price": "140",
+            "owner": "Board"},
+    }
+
+    player_location = {'player1': 1, 'player2': 1, 'player3': 5}
+    # endregion
+    while True:
+        # region menu
+        print(f'''
+    ##########################
+    ###  Jogo do Monopólio ###
+    ##########################
+
+    1 - Novo Jogo
+    2 - Histórico de jogos
+    9 - Sair
+    ''')
+
+        # endregion menu
+        option = int(input(f'Selectione uma opção: '))
+        if option == 1:
+            while True:
+                if len(players) < 2:
+                    player_name_1 = input("What is the player 1 name? ")
+                    player_name_2 = input("What is the player 2 name? ")
+                    player_name_3 = input("What is the player 3 name? ")
+                    player_name = (player_name_1, player_name_2, player_name_3)
+                    run_player = reg_player(players, player_name)
+                    print(run_player)
+                    continue
+
+        elif option == 2:
+            print('test 2')
+        elif option == 9:
+            quit()
+        else:
+            print(f'Opção selecionada não é válida!')
